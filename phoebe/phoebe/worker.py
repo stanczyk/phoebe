e77fb7052b388266174633820cd4a35a8ece70ef
jarek on vagrant <jaroslaw.stanczyk@upwr.edu.pl>
2017-11-30 10:44:39 +0100

    09: phoebe cont.

--- phoebe/phoebe/worker.py
+++ phoebe/phoebe/worker.py
@@ -9,14 +9,16 @@ in the module:
 
 Copyright 2017 Jaroslaw Stanczyk, e-mail: jaroslaw.stanczyk@upwr.edu.pl
 """
+# pylama: ignore=C901
+# pylint: disable=relative-import
 import sys
 from err import Err
-from inf import Inf
 from parser import Parser
 
 
 class Worker(object):
 	"""Worker class"""
+	# pylint: disable=invalid-name, missing-docstring, too-many-instance-attributes
 	def __init__(self):
 		self.parser = None
 		self.u = []
@@ -43,12 +45,14 @@ class Worker(object):
 				vec.append(self.parser.yaml.get_key(my_dic[j]))
 		for i in range(0, len(tmp)):
 			self.x.append('x_' + str(i + 1))
+		return Err.NOOP
 
 	def show_vectors(self):
 		print '== VECTORS ================='
 		self.print_vector('u(k)', self.u)
 		self.print_vector('x(k)', self.x)
 		self.print_vector('y(k)', self.y)
+		return Err.NOOP
 
 	@staticmethod
 	def print_vector(name, vector):
@@ -56,6 +60,7 @@ class Worker(object):
 		for i in range(0, len(vector)):
 			print vector[i],
 		print ']\'' if len(vector) > 1 else ']'
+		return Err.NOOP
 
 	def prepare_mapping(self):
 		for i in ['input', 'prod-unit', 'output']:
@@ -63,66 +68,80 @@ class Worker(object):
 			for j in range(0, self.parser.yaml.get_len(my_dic)):
 				name = self.parser.yaml.get_key(my_dic[j])
 				self.mapping[name] = j
+		return Err.NOOP
 
 	def create_matrix(self, matrix, v1, v2):
 		for _ in range(0, self.parser.yaml.get_len(v1)):
 			tmp = []
 			for __ in range(0, self.parser.yaml.get_len(v2)):
-				tmp.append('e')
+				tmp.append('-')
 			matrix.append(tmp)
+		return Err.NOOP
 
 	def fill_matrix(self, matrix, my_dic):
 		for i in range(0, self.parser.yaml.get_len(my_dic)):
 			key = self.parser.yaml.get_key(my_dic[i])
-			int_dic = self.parser.yaml.get_value(my_dic[i], key)
-			op_time, connect, tr_time = self.parser.get_details(int_dic)
+			op_time, connect, tr_time = self.parser.get_details(self.parser.yaml.get_value(my_dic[i], key))
 			tmp = ''
 			if op_time:
 				tmp += op_time
-			if matrix in ['A0', 'B0']:
+			if matrix in [self.A0, self.B0]:
 				if connect and connect[0] != 'y':
-					j = self.mapping[connect]
 					if tr_time:
 						tmp += tr_time
-					if matrix == 'A0':
-						self.A0[j][i] = tmp
-					if matrix == 'B0':
-						self.B0[j][i] = tmp
-			if matrix in ['A1']:
+					j = self.mapping[connect]
+					matrix[j][i] = tmp
+			if matrix in [self.A1]:
 				if op_time:
 					j = self.mapping[key]
-					self.A1[j][i] = tmp
-			if matrix in ['C']:
+					matrix[j][i] = tmp
+			if matrix in [self.C]:
 				if connect and connect[0] == 'y':
-					j = self.mapping[connect]
 					if tr_time:
 						tmp += tr_time
-					self.C[j][i] = tmp
+					j = self.mapping[connect]
+					matrix[j][i] = tmp
+		return Err.NOOP
 
 	def matrix_preparation(self):
 		self.create_matrix(self.B0, self.x, self.u)
-		self.fill_matrix('B0', self.parser.yaml.get_value(self.parser.content_yaml, 'input'))
+		self.fill_matrix(self.B0, self.parser.yaml.get_value(self.parser.content_yaml, 'input'))
 		prod_unit = self.parser.yaml.get_value(self.parser.content_yaml, 'prod-unit')
 		self.create_matrix(self.A0, self.x, self.x)
-		self.fill_matrix('A0', prod_unit)
+		self.fill_matrix(self.A0, prod_unit)
 		self.create_matrix(self.A1, self.x, self.x)
-		self.fill_matrix('A1', prod_unit)
+		self.fill_matrix(self.A1, prod_unit)
 		self.create_matrix(self.C, self.y, self.x)
-		self.fill_matrix('C', prod_unit)
+		self.fill_matrix(self.C, prod_unit)
+		return Err.NOOP
+
+	def show_details_3(self):
+		print '== DETAILS 3 ==============='
+		print self.mapping
+		self.show_matrices()
 
 	def show_matrices(self):
 		print '== MATRICES ================'
 		for i in [self.A0, self.A1, self.B0, self.C]:
+			if i == self.A0:
+				print 'A0 =',
+			if i == self.A1:
+				print 'A1 =',
+			if i == self.B0:
+				print 'B0 =',
+			if i == self.C:
+				print 'C  =',
 			print i
+		return Err.NOOP
 
 	def main_work(self):
 		self.prepare_vectors()
 		if self.parser.args['--vectors']:
 			self.show_vectors()
 		self.prepare_mapping()
-		print self.mapping
 		self.matrix_preparation()
-		self.show_matrices()
+		if self.parser.args['--details-3']:
+			self.show_details_3()
 		return Err.NOOP
 
 	def main(self):
