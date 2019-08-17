4be9a0a6da51baa39b0c2b12aca8e2ff90db2f52
jarek at home <j.stanczyk@hotmail.com>
2019-08-17 20:12:39 +0200

    phoebe/worker.py

--- phoebe/worker.py
+++ phoebe/worker.py
@@ -37,17 +37,23 @@ class Worker(object):
 		# self.values = None
 		pass
 
+	"""
 	@click.command()
-	@click.argument('filename')
+	@click.option('-v', '--version', is_flag=True, help='Displays version information and exit.')
 	@click.option('--file', is_flag=True, help='Show information from FILENAME.')
 	@click.option('--dont', is_flag=True, help='State space model is not generated.')
-	def get_cmdline_params(filename, file, dont):
-		"""The max-plus algebraic state space model generator."""
+	@click.argument('filename')
+	def get_cmdline_params(version, file, dont, filename):
+		
+		The max-plus algebraic state space model generator.\n
+		Copyright: (c) 2017-2019 Jarosław Stańczyk <j.stanczyk@hotmail.com>
+		
+		if version:
+			click.echo("We are in the verbose mode.")
 		print("filename ", filename)
 		print("file ", file)
 		print("gen ", dont)
 
-	"""
 	def init_parser(self):
 		self.parser = Parser()
 
@@ -382,13 +388,47 @@ class Worker(object):
 	"""
 
 
+@click.group()
+def cli1():
+	pass
+
+
+@cli1.command()
+@click.option('-v', '--version', is_flag=True, help='Displays version information and exit.')
+def cmd1(version):
+	if version:
+		click.echo("We are in the verbose mode.")
+
+
+@click.group()
+def cli2():
+	pass
+
+
+@cli2.command()
+@click.option('--file', is_flag=True, help='Show information from FILENAME.')
+@click.option('--dont', is_flag=True, help='State space model is not generated.')
+@click.argument('filename')
+def cmd2(file, dont, filename):
+	"""
+	The max-plus algebraic state space model generator.\n
+	Copyright: (c) 2017 - 2019 Jarosław Stańczyk <j.stanczyk@hotmail.com>
+	"""
+	print("filename ", filename)
+	print("file ", file)
+	print("gen ", dont)
+
+
+cli = click.CommandCollection(sources=[cli1, cli2])
+
+
 def main():
-	proces = Worker()
-	proces.get_cmdline_params()
+	cli()
+	# proces = Worker()
+	# get_cmdline_params()
 	# proces.init_parser()
 	# proces.parser.main()
 	# sys.exit(proces.main_work())
-	pass
 
 
 if __name__ == '__main__':
