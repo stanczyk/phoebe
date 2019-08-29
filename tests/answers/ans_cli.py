6a4842a59a3368a75686ce756be43c2c1954520d
jarek at tieto <jaroslaw.stanczyk@tieto.com>
2019-08-29 14:51:04 +0200

    tests

--- tests/answers/ans_cli.py
+++ tests/answers/ans_cli.py
@@ -24,8 +24,7 @@ Options:\n\
 \n\
 Commands:\n\
   latex   Generate latex description.\n\
-  matlab  Generate max-plus matlab model.\n\
-'
+  matlab  Generate max-plus matlab model.\n'
 
 ANS_FILE = '\
 == INPUT FILE ==============\n\
@@ -61,7 +60,102 @@ values:\n\
   t_{1,3}: 1\n\
   t_{2,3}: 0\n\
   t_{3,4}: 0\n\
-\n\
-'
+\n'
+
+ANS_VEC1 = '\
+== VECTORS =================\n\
+u(k) = [ u_1(k) u_2(k) ]\'\n\
+x(k) = [ x_1(k) x_2(k) x_3(k) ]\'\n\
+y(k) = [ y_1(k) ]\'\n\
+\n'
+
+ANS_VEC2 = '\
+== VECTORS =================\n\
+u(k) = []\n\
+x(k) = []\n\
+y(k) = [ y_1(k) ]\'\n\
+\n'
+
+ANS_DET1_1 = '\
+== DETAILS 1 ===============\n\
+input: [\
+{\'u_1\': {\'connect\': {\'M_1\': {\'tr-time\': \'t_{0,1}\', \'buffers\': \'-\'}}, \'op-time\': \'0\'}}, \
+{\'u_2\': {\'connect\': {\'M_2\': {\'tr-time\': \'t_{0,2}\', \'buffers\': \'-\'}}, \'op-time\': \'0\'}}]\n\
+prod-unit: [\
+{\'M_1\': {\'connect\': {\'M_3\': {\'tr-time\': \'t_{1,3}\', \'buffers\': \'-\'}}, \'op-time\': \'d_1\'}}, \
+{\'M_2\': {\'connect\': {\'M_3\': {\'tr-time\': \'t_{2,3}\', \'buffers\': \'-\'}}, \'op-time\': \'d_2\'}}, \
+{\'M_3\': {\'connect\': {\'y\': {\'tr-time\': \'t_{3,4}\', \'buffers\': \'-\'}}, \'op-time\': \'d_3\'}}]\n\
+output: [\
+{\'y\': {}}]\n\
+values: \
+{\'t_{0,1}\': 2, \'t_{0,2}\': 0, \'t_{1,3}\': 1, \'t_{2,3}\': 0, \'t_{3,4}\': 0, \'d_1\': 5, \'d_2\': 6, \'d_3\': 3}\n\
+\n'
+
+ANS_DET1_2 = '\
+== DETAILS 1 ===============\n\
+input: None\n\
+prod-unit: None\n\
+output: [{\'y_1\': {}}]\n\
+values: None\n\
+\n'
+
+ANS_DET2_1 = '\
+== DETAILS 2 ===============\n\
+input: \n\
+  u_1\n\
+    op-time: 0\n\
+    connect:\n\
+      M_1\n\
+        tr-time: t_{0,1}\n\
+        buffers: -\n\
+  u_2\n\
+    op-time: 0\n\
+    connect:\n\
+      M_2\n\
+        tr-time: t_{0,2}\n\
+        buffers: -\n\
+prod-unit: \n\
+  M_1\n\
+    op-time: d_1\n\
+    connect:\n\
+      M_3\n\
+        tr-time: t_{1,3}\n\
+        buffers: -\n\
+  M_2\n\
+    op-time: d_2\n\
+    connect:\n\
+      M_3\n\
+        tr-time: t_{2,3}\n\
+        buffers: -\n\
+  M_3\n\
+    op-time: d_3\n\
+    connect:\n\
+      y\n\
+        tr-time: t_{3,4}\n\
+        buffers: -\n\
+output: \n\
+  y\n\
+    op-time: -\n\
+    connect: -\n\
+values:\n\
+  d_1: 5\n\
+  d_2: 6\n\
+  d_3: 3\n\
+  t_{0,1}: 2\n\
+  t_{0,2}: 0\n\
+  t_{1,3}: 1\n\
+  t_{2,3}: 0\n\
+  t_{3,4}: 0\n\
+\n'
+
+ANS_DET2_2 = '\
+== DETAILS 2 ===============\n\
+input: \n\
+prod-unit: \n\
+output: \n\
+  y_1\n\
+    op-time: -\n\
+    connect: -\n\
+\n'
 
 # end.
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,8 +14,13 @@ import unittest
 # from io import StringIO
 # import mock
 from click.testing import CliRunner
-# import phoebe.cli
-from tests.answers.ans_cli import ANS_HLP, ANS_FILE
+from tests.answers.ans_cli import ANS_HLP, ANS_FILE, ANS_VEC1, ANS_VEC2, ANS_DET1_1, ANS_DET1_2, ANS_DET2_1, ANS_DET2_2
+
+lib_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../phoebe')
+if lib_path not in sys.path:
+	sys.path.append(lib_path)
+import phoebe.cli
+
 
 # pylint: disable=missing-docstring
 # https://click.palletsprojects.com/en/7.x/testing/
@@ -26,36 +31,56 @@ class TestCli(unittest.TestCase):
 	""" class for testing *Generator* """
 
 	def setUp(self):
-		pass
+		self.runner = CliRunner()
 
 	def tearDown(self):
 		pass
 
 	def test_cli_help(self):
-		lib_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../phoebe')
-		if lib_path not in sys.path:
-			sys.path.append(lib_path)
-		import phoebe.cli
-
-		runner = CliRunner()
-		result = runner.invoke(phoebe.cli.cli, ['-h'])
+		result = self.runner.invoke(phoebe.cli.cli, ['-h'])
 		self.assertEqual(result.exit_code, 0)
 		# print(result.output)
 		self.assertEqual(result.output, ANS_HLP)
 
-	def test_cli_show_file(self):
-		lib_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../phoebe')
-		if lib_path not in sys.path:
-			sys.path.append(lib_path)
-		import phoebe.cli
-
-		runner = CliRunner()
-		result = runner.invoke(phoebe.cli.cli, ['--showfile'])
+	def test_cli_showfile(self):
+		result = self.runner.invoke(phoebe.cli.cli, ['--showfile'])
 		self.assertEqual(result.exit_code, 2)
-		result = runner.invoke(phoebe.cli.cli, ['--showfile', 'tests/samples/cli1.yml'])
+		result = self.runner.invoke(phoebe.cli.cli, ['--showfile', 'tests/samples/cli1.yml'])
 		self.assertEqual(result.exit_code, 0)
 		# print(result.output)
 		self.assertEqual(result.output, ANS_FILE)
+
+	def test_cli_vectors(self):
+		result = self.runner.invoke(phoebe.cli.cli, ['--vectors'])
+		self.assertEqual(result.exit_code, 2)
+		result = self.runner.invoke(phoebe.cli.cli, ['--vectors', 'tests/samples/cli1.yml'])
+		self.assertEqual(result.exit_code, 0)
+		# print(result.output)
+		self.assertEqual(result.output, ANS_VEC1)
+		result = self.runner.invoke(phoebe.cli.cli, ['--vectors', 'tests/samples/cli2.yml'])
+		self.assertEqual(result.exit_code, 0)
+		# print(result.output)
+		self.assertEqual(result.output, ANS_VEC2)
+
+	def test_cli_det1(self):
+		result = self.runner.invoke(phoebe.cli.cli, ['--det1', 'tests/samples/cli1.yml'])
+		self.assertEqual(result.exit_code, 0)
+		# print(result.output)
+		self.assertEqual(result.output, ANS_DET1_1)
+		result = self.runner.invoke(phoebe.cli.cli, ['--det1', 'tests/samples/cli2.yml'])
+		self.assertEqual(result.exit_code, 0)
+		# print(result.output)
+		self.assertEqual(result.output, ANS_DET1_2)
+
+	def test_cli_det2(self):
+		result = self.runner.invoke(phoebe.cli.cli, ['--det2', 'tests/samples/cli1.yml'])
+		self.assertEqual(result.exit_code, 0)
+		# print(result.output)
+		self.assertEqual(result.output, ANS_DET2_1)
+		result = self.runner.invoke(phoebe.cli.cli, ['--det2', 'tests/samples/cli2.yml'])
+		self.assertEqual(result.exit_code, 0)
+		print(result.output)
+		self.assertEqual(result.output, ANS_DET2_2)
 
 
 def main():
