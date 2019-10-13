b69833524775f3676d55a7ec18fd8c0cae0ecd83
jarek at home <j.stanczyk@hotmail.com>
2019-10-13 22:35:31 +0200

    examples to tests have been added

--- tests/test_spc.py
+++ tests/test_spc.py
@@ -12,8 +12,7 @@ import glob
 import os
 import sys
 import unittest
-# from io import StringIO
-# import mock
+from builtins import staticmethod
 from freezegun import freeze_time  # https://github.com/spulec/freezegun
 from click.testing import CliRunner
 
@@ -33,21 +32,34 @@ class TestSpc(unittest.TestCase):
 	def tearDown(self):
 		pass
 
+	@staticmethod
+	def check_file(file, content):
+		if not os.path.isfile(file):
+			writer = open(file, 'w')
+			writer.write(content)
+			writer.close()
+
 	@freeze_time("2019-10-13 15:46:20")
 	def test_specs(self):
 		for file in glob.glob('specs/desc*.yml'):
 			base = os.path.basename(os.path.splitext(file)[0])
-			print(base)
-			result1 = self.runner.invoke(phoebe.cli.cli, [file, 'matlab'])
-			self.assertEqual(result1.exit_code, Err.NOOP)
-			# print(result1.output)
-			with open('tests/answers/matlab/' + base + '.m') as base_reader1:
-				self.assertEqual(result1.output, base_reader1.read())
-			result2 = self.runner.invoke(phoebe.cli.cli, [file, 'latex'])
-			self.assertEqual(result2.exit_code, Err.NOOP)
-			# print(result2.output)
-			with open('tests/answers/latex/' + base + '.tex') as base_reader2:
-				self.assertEqual(result2.output, base_reader2.read())
+			# print(base)
+			# matlab
+			result = self.runner.invoke(phoebe.cli.cli, [file, 'matlab'])
+			self.assertEqual(result.exit_code, Err.NOOP)
+			# print(result.output)
+			file_path = 'tests/answers/matlab/' + base + '.m'
+			self.check_file(file_path, result.output)
+			with open(file_path) as reader:
+				self.assertEqual(result.output, reader.read())
+			# latex
+			result = self.runner.invoke(phoebe.cli.cli, [file, 'latex'])
+			self.assertEqual(result.exit_code, Err.NOOP)
+			# print(result.output)
+			file_path = 'tests/answers/latex/' + base + '.tex'
+			self.check_file(file_path, result.output)
+			with open(file_path) as reader:
+				self.assertEqual(result.output, reader.read())
 
 
 def main():
